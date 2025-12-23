e2ee006d378c2d102392fde15dd00d2c7bf542e7
ifrancro <ifrancro@gmail.com>
2025-12-23 11:06:19 -0400

    Creacion Proyecto

---
+++ src/main/java/com/example/herbalife_clubes/HerbalifeClubesApplication.java
@@ -0,0 +1,13 @@
+package com.example.herbalife_clubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbalifeClubesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerbalifeClubesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/herbalife_clubes/HerbalifeClubesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herbalife_clubes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbalifeClubesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
