604eb9d893a6ae2fec8eabe732e0bb0e7a2f24ff
pizzatoev <evatoronogales@gmail.com>
2026-01-23 16:57:05 -0400

    cambios en el cors

--- src/main/java/com/example/herbalife_clubes/config/CorsConfig.java
+++ src/main/java/com/example/herbalife_clubes/config/CorsConfig.java
@@ -3,8 +3,8 @@ package com.example.herbalife_clubes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -12,7 +12,7 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
@@ -34,7 +34,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        return source;
     }
 }
 
